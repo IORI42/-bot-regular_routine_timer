cad9c96c229ac26c096f0c2d26f38970a4ef5583
IORI42 <iori.42.0001@gmail.com>
2022-03-11 06:18:57 +0900

    list_command

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,31 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
+
+#指定した時間経過すると切断する
 @bot.command()
-async def ping(ctx):
+async def for_time(ctx):
     await ctx.send('pong')
 
+#毎日のアラームを設定する
+@bot.command()
+async def set(ctx):
+    await ctx.send('pong')
+
+#今日だけ有効なアラームを設定する
+@bot.command()
+async def today(ctx):
+    await ctx.send('pong')
+
+#設定しているアラームの一覧を表示する
+@bot.command()
+async def show(ctx):
+    await ctx.send('pong')
+
+#設定しているアラームを削除する
+@bot.command()
+async def delete(ctx):
+    await ctx.send('pong')
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
