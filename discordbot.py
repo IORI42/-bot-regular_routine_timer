a9ff2d6e26afed0e60358fc504464a167eb0aaf2
IORI42 <iori.42.0001@gmail.com>
2022-03-11 07:45:11 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix='/')
 
 #指定した時間経過すると切断する
 @bot.command()
-async def for_time(ctx,min):
+async def for_time(ctx,min: int):
     time = datetime.datetime.now()
     time = time + datetime.timedelta(minutes=min)
     await ctx.send(time)
