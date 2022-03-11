77f1c3a7a18df77b12a1406e9bbf2528ae2c8c44
IORI42 <iori.42.0001@gmail.com>
2022-03-11 11:21:33 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,19 @@ from os import getenv
 import traceback
 import datetime
 
+import discord
+
 bot = commands.Bot(command_prefix='/')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+client = MyClient()
+client.run('my token goes here')
 
 #指定した時間経過すると切断する
 @bot.command()
